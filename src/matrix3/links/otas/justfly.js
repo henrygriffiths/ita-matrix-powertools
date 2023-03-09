ff2bfd14958d50d138545622a9bb22d018d960c5
Adam Hwang <github@adamhwang.com>
2023-03-08 23:36:16 -0600

    Added Justfly

---
+++ src/matrix3/links/otas/justfly.js
@@ -0,0 +1,28 @@
+import { register } from "..";
+import { currentItin } from "../../../matrix5/parse/itin";
+import { buildQueryString } from "./travix";
+
+function print() {
+  // 0 = Economy; 1=Premium Economy; 2=Business; 3=First
+  var cabins = ["Economy", "Economy", "Business", "First"];
+  var createUrl = edition =>
+    `https://www.justfly.com/checkout/gdeeplink?${buildQueryString(
+      currentItin.cur || "USD",
+      edition.country,
+      edition.lang,
+      cabins
+    )}`;
+
+  // get edition
+  const url = createUrl({ lang: "en", country: "US" });
+  if (!url) {
+    return;
+  }
+
+  return {
+    url,
+    title: "Justfly"
+  };
+}
+
+register("otas", print);
