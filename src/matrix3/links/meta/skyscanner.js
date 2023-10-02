e10b8f786c70928321e2d546f037c3060bbb448b
Adam Hwang <github@adamhwang.com>
2023-10-02 13:52:51 -0500

    Added all skyscanner sites

--- src/matrix3/links/meta/skyscanner.js
+++ src/matrix3/links/meta/skyscanner.js
@@ -5,20 +5,51 @@ import { currentItin, getCurrentSegs } from "../../../matrix5/parse/itin";
 import { printNotification, to2digits } from "../../utils";
 
 const editions = [
-  { name: "Skyscanner.com", market: "US" },
-  { name: "Skyscanner.de", market: "DE" },
-  { name: "Skyscanner.it", market: "IT" },
-  { name: "Skyscanner.es", market: "ES" },
-  { name: "Skyscanner.co.uk", market: "UK" },
-  { name: "Skyscanner.dk", market: "DK" },
-  { name: "Skyscanner.mx", market: "MX" },
-  { name: "Skyscanner.fi", market: "FI" },
-  { name: "Skyscanner.fr", market: "FR" },
-  { name: "Skyscanner.no", market: "NO" },
-  { name: "Skyscanner.nl", market: "NL" },
-  { name: "Skyscanner.pt", market: "PT" },
-  { name: "Skyscanner.se", market: "SE" },
-  { name: "Skyscanner.ru", market: "RU" },
+  { name: "www.skyscanner.com", market: "US" },
+  { name: "www.skyscanner.ae", market: "AE" },
+  { name: "www.skyscanner.at", market: "AT" },
+  { name: "www.skyscanner.com.au", market: "AU" },
+  { name: "www.skyscanner.com.br", market: "BR" },
+  { name: "www.skyscanner.ca", market: "CA" },
+  { name: "www.skyscanner.ch", market: "CH" },
+  { name: "www.tianxun.com", market: "CN" },
+  { name: "www.espanol.skyscanner.com", market: "CO" },
+  { name: "www.skyscanner.cz", market: "CZ" },
+  { name: "www.skyscanner.de", market: "DE" },
+  { name: "www.skyscanner.dk", market: "DK" },
+  { name: "www.skyscanner.es", market: "ES" },
+  { name: "www.skyscanner.fi", market: "FI" },
+  { name: "www.skyscanner.fr", market: "FR" },
+  { name: "www.skyscanner.gg", market: "GG" },
+  { name: "gr.skyscanner.com", market: "GR" },
+  { name: "www.skyscanner.com.hk", market: "HK" },
+  { name: "www.skyscanner.hu", market: "HU" },
+  { name: "www.skyscanner.co.id", market: "ID" },
+  { name: "www.skyscanner.ie", market: "IE" },
+  { name: "www.skyscanner.co.il", market: "IL" },
+  { name: "www.skyscanner.co.in", market: "IN" },
+  { name: "www.skyscanner.it", market: "IT" },
+  { name: "www.skyscanner.jp", market: "JP" },
+  { name: "www.skyscanner.co.kr", market: "KR" },
+  { name: "www.skyscanner.com.mx", market: "MX" },
+  { name: "www.skyscanner.com.my", market: "MY" },
+  { name: "www.skyscanner.nl", market: "NL" },
+  { name: "www.skyscanner.no", market: "NO" },
+  { name: "www.skyscanner.co.nz", market: "NZ" },
+  { name: "www.skyscanner.com.ph", market: "PH" },
+  { name: "www.skyscanner.pl", market: "PL" },
+  { name: "www.skyscanner.pt", market: "PT" },
+  { name: "www.skyscanner.ro", market: "RO" },
+  { name: "ru.skyscanner.com", market: "RU" },
+  { name: "www.skyscanner.com.sa", market: "SA" },
+  { name: "www.skyscanner.se", market: "SE" },
+  { name: "www.skyscanner.com.sg", market: "SG" },
+  { name: "www.skyscanner.co.th", market: "TH" },
+  { name: "www.skyscanner.com.tr", market: "TR" },
+  { name: "www.skyscanner.com.tw", market: "TW" },
+  { name: "www.skyscanner.com.ua", market: "UA" },
+  { name: "www.skyscanner.net", market: "UK" },
+  { name: "www.skyscanner.com.vn", market: "VN" },
 ];
 
 var cabins = ["", "premiumeconomy", "business", "first"];
@@ -44,8 +75,8 @@ function print(method) {
   const cabin =
     cabins[getCabin(Math.min(...getCurrentSegs().map((seg) => seg.cabin)))];
 
-  var createUrl = function (market) {
-    var url = "http://www.skyscanner.com/transport/d/";
+  var createUrl = function (edition) {
+    var url = `http://${edition.name}/transport/d/`;
 
     // Add the segments:
     url += segs
@@ -70,21 +101,17 @@ function print(method) {
     // Add cabin / class of service:
     url += "&cabinclass=" + cabin;
     // Add locale ("market"):
-    url += "&ref=day-view&market=" + market;
+    url += "&ref=day-view&market=" + edition.market;
 
     return url;
   };
-  var url = createUrl("US");
+  var url = createUrl(editions[0]);
   var extra =
     ' <span class="pt-hover-container">[+]<span class="pt-hover-menu">';
   extra += editions
     .map(function (obj, i) {
       return (
-        '<a href="' +
-        createUrl(obj.market) +
-        '" target="_blank">' +
-        obj.name +
-        "</a>"
+        '<a href="' + createUrl(obj) + '" target="_blank">' + obj.name + "</a>"
       );
     })
     .join("<br/>");
