991fdb285331b27a56d0ddc8eabce8815d513947
Phil Dumbreck <29596014+padbk@users.noreply.github.com>
2024-09-06 21:47:37 +0100

    Update ita-matrix-powertools.user.js

--- ita-matrix-powertools.user.js
+++ ita-matrix-powertools.user.js
@@ -2,7 +2,7 @@
 // @name ITA Matrix Powertools
 // @namespace https://github.com/adamhwang/ita-matrix-powertools
 // @description Adds new features and builds fare purchase links for ITA Matrix
-// @version 0.55.14
+// @version 0.55.15
 // @icon https://raw.githubusercontent.com/adamhwang/ita-matrix-powertools/master/icons/icon32.png
 // @require https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
 // @grant GM.getValue
@@ -6830,7 +6830,7 @@ function boolToEnabled(value) {
 const appSettings = {
     isUserscript: !(typeof GM === "undefined" || typeof GM.info === "undefined"),
     itaLanguage: "en",
-    version: "0.55.14",
+    version: "0.55.15",
     retrycount: 1,
     laststatus: "",
     scriptrunning: 1,
